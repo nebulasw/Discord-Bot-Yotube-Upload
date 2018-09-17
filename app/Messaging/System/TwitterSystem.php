30e613f25a8572d2dc8c3476a4c6e6ca6fb768b9
Woeler <woeler@woeler.eu>
2018-09-17 16:22:54 +0200

    [TASK] Add logging to Twitter

--- app/Messaging/System/TwitterSystem.php
+++ app/Messaging/System/TwitterSystem.php
@@ -11,6 +11,15 @@ class TwitterSystem extends AbstractMessagingSystem
 
     public function send(YoutubeVideo $video, array $configuration): bool
     {
+        if (empty($configuration['consumerKey'])
+            || empty($configuration['consumerSecret'])
+            || empty($configuration['accessToken'])
+            || empty($configuration['accessTokenSecret'])) {
+            $this->logger->error('Your Twitter configuration is incomplete');
+
+            return false;
+        }
+
         $twitter = new Twitter($configuration['consumerKey'], $configuration['consumerSecret'], $configuration['accessToken'], $configuration['accessTokenSecret']);
         $message = $video->getChannelName() . ' posted a new video! ' . $video->getTitle() . PHP_EOL . $video->getUrl();
         $twitter->send($message);
